0e9b07a18162644ee70091446771951ca510653a
Oliver Hamlet <oliver.hamlet@gmail.com>
2017-01-15 10:03:48 +0000

    Define NO_COMPRESSION when building Boost
    To remove the unecessary libbz2 and zlib dependencies in Iostreams.

--- install_boost.py
+++ install_boost.py
@@ -88,6 +88,7 @@ def build_boost(boost_root, address_model, libraries):
         'runtime-link={}'.format(runtime_link),
         'variant=release',
         'address-model={}'.format(address_model),
+        'define=NO_COMPRESSION=1',
         '--stagedir={}'.format(os.path.join('stage', address_model))
     ] + os_arguments + [ '--with-{}'.format(library) for library in libraries ]
 
