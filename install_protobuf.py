5f3009840751c6514c1d90b6b350e557922d15dc
Oliver Hamlet <oliver.hamlet@gmail.com>
2017-10-25 18:19:59 +0100

    Add script for installing Protocol Buffers

---
+++ install_protobuf.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+import shutil
+import subprocess
+import sys
+import tempfile
+import urllib
+
+def build(source_dir, output_dir):
+    build_dir = os.path.join(source_dir, 'cmake')
+
+    cmake_command = [
+        'cmake',
+        '.',
+        '-DCMAKE_INSTALL_PREFIX={}'.format(output_dir)
+    ]
+
+    subprocess.check_call(cmake_command, cwd = build_dir)
+
+    subprocess.check_call(['make'], cwd = build_dir)
+
+    subprocess.check_call(['make', 'install'], cwd = build_dir)
+
+def extract_archive(archive_path):
+    print('Extracting {}...'.format(archive_path))
+
+    output_path = os.path.dirname(archive_path)
+
+    extracted_path = get_extracted_path(archive_path)
+    if os.path.exists(extracted_path):
+        shutil.rmtree(extracted_path)
+
+    if os.name == 'nt':
+        subprocess.check_call(['7z', 'x', archive_path, u'-o{}'.format(output_path)], cwd = output_path)
+    else:
+        subprocess.check_call(['tar', 'xf', archive_path], cwd = output_path)
+
+    return extracted_path
+
+def get_extracted_path(archive_path):
+    filename = 'protobuf-{}'.format(
+        os.path.splitext(os.path.splitext(archive_path)[0])[0].split('-')[-1]
+    )
+    return os.path.join(
+        os.path.dirname(archive_path),
+        filename
+    )
+
+def get_archive_name(version):
+    return 'protobuf-cpp-{}.tar.gz'.format(version)
+
+def get_url(version):
+    return 'https://github.com/google/protobuf/releases/download/v{}/{}'.format(
+        version,
+        get_archive_name(version)
+    )
+
+def download(version, output_path):
+    print('Downloading Protocol Buffers v{}...'.format(version))
+
+    if os.path.exists(output_path):
+        os.remove(output_path)
+
+    urllib.urlretrieve(get_url(version), output_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = 'Download and build Protocol Buffers.')
+    parser.add_argument('--output-directory', '-o', required = True)
+    parser.add_argument('--target-version', '-t', required = True)
+
+    arguments = parser.parse_args()
+
+    output_dir = arguments.output_directory
+    if os.path.exists(output_dir):
+        print('Protocol Buffers already installed at {}'.format(output_dir))
+        sys.exit(0)
+
+    print('Protocol Buffers not found at {}, installing...'.format(output_dir))
+
+    archive_path = os.path.join(tempfile.gettempdir(), get_archive_name(arguments.target_version))
+
+    download(arguments.target_version, archive_path)
+
+    extracted_path = extract_archive(archive_path)
+
+    build(extracted_path, output_dir)
+
+    os.remove(archive_path)
+    shutil.rmtree(extracted_path)
